d4a7b0427b9bba95faf977b95777f3122034e808
Saad Amir <Saadamir070@gmail.com>
2024-08-30 03:33:31 +0500

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeholder1 from "../assets/placeholder1.png";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeholder1; // if no image, use default placeholder
   const target = "media/";
   const index = url.indexOf(target) + target.length; // url give lenght till start of target, so we added target' length to reach end of our target
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
