093573812def2d60ecbbb310d9f380bbdbc6055a
Saad Amir <Saadamir070@gmail.com>
2024-08-29 14:01:29 +0500

    Getting Optimized Images

---
+++ src/services/image-url.ts
@@ -0,0 +1,7 @@
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length; // url give lenght till start of target, so we added target' length to reach end of our target
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
