c8337f966f12412a5672086fe683adedf4ef2d89
Saad Amir <Saadamir070@gmail.com>
2024-08-29 13:45:18 +0500

    Displaying Critic Score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,6 +12,7 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
+  metacritic: number;
   parent_platforms: { platform: Platform }[]; //parent_platforms is array of object where each object has a property called platform
 }
 
@@ -30,7 +31,7 @@ const useGames = () => {
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
-        if (err instanceof CanceledError) return;
+        if (err instanceof CanceledError) return; // to ignore the 1st render by strict mode and only consider 2nd one
         setError(err.message);
       });
 
