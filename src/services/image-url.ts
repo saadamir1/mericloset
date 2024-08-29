f8ebb5289d8f394c7d05022a81b116c86fef8059
Saad Amir <Saadamir070@gmail.com>
2024-08-30 03:20:06 +0500

    Sort the Games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return "";
   const target = "media/";
   const index = url.indexOf(target) + target.length; // url give lenght till start of target, so we added target' length to reach end of our target
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
