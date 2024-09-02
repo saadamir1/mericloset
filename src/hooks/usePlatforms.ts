8693feb28db054e9aa5a5e22a469ca11d52089ae
Saad Amir <Saadamir070@gmail.com>
2024-09-02 16:31:38 +0500

    fetching genres and platforms using react query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,22 @@
-import useData from "./useData";
+import apiClient from "../services/api-client";
+import useData, { FetchResponse } from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
 
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
-
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    //for 24 hours stored/cached initial data will be used rather than sending request time. will be refreshed after 24hours.
+    staleTime: 24 * 60 * 60 * 1000, //24hours
+    initialData: { count: platforms.length, results: platforms },
+  });
 export default usePlatforms;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import { createRoot } from "react-dom/client";
 import theme from "./theme.ts";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+ReactQueryDevtools;
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,7 +16,10 @@ createRoot(document.getElementById("root")!).render(
       <ColorModeScript
         initialColorMode={theme.config.initialColorMode}
       ></ColorModeScript>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>
 );
