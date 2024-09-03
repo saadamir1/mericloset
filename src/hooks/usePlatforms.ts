f6e49afbc57228f6582fe4d505e8ddb43a228337
Saad Amir <Saadamir070@gmail.com>
2024-09-03 21:30:18 +0500

    refactoring: create a reusable API client

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,9 @@
-import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import APIClient from "../services/api-client";
+
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -11,10 +13,7 @@ export interface Platform {
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Platform>>("/platforms/lists/parents")
-        .then((res) => res.data),
+    queryFn: apiClient.getAll,
     //for 24 hours stored/cached initial data will be used rather than sending request time. will be refreshed after 24hours.
     staleTime: 24 * 60 * 60 * 1000, //24hours
     initialData: { count: platforms.length, results: platforms },
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,28 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "30560d91abe14549b4cdbc4cb3cd1225",
   },
 });
+
+class apiClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default apiClient;
