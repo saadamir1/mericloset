f80b9e266803ccbeb4c06d13d02a0670988218fb
Saad Amir <Saadamir070@gmail.com>
2024-09-12 16:09:59 +0500

    refactor entity interfaces

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,7 +2,7 @@ import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import useGames from "../hooks/useGames";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleon from "./GameCardSkeleton";
