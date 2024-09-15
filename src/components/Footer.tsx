fd603ca74a388baf60984373655d97c055555f5e
Saad Amir <Saadamir070@gmail.com>
2024-09-15 19:20:18 +0500

    Include footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import { Box, Text, Link } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box as="footer" bg="gray.700" color="white" py={2} textAlign="center">
+      <Text>
+        &copy; {new Date().getFullYear()} Game Hub. All rights reserved.
+      </Text>
+      <Box mt={2}>
+        <Link href="#" mx={2}>
+          Privacy Policy
+        </Link>
+        <Link href="#" mx={2}>
+          Terms of Service
+        </Link>
+        <Link href="#" mx={2}>
+          Contact Us
+        </Link>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
+import Footer from "../components/Footer";
 
 const layout = () => {
   return (
     <>
-      <NavBar />
-      <Box padding={5}>
-        <Outlet />
-      </Box>
+      <Flex direction="column" minHeight="100vh">
+        <NavBar />
+        <Box flex="1" padding={5}>
+          <Outlet />
+        </Box>
+        <Footer />
+      </Flex>
     </>
   );
 };
