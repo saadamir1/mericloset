5ea4d2ce04ea1b634f5f9097a047978eb5b7a0e2
Saad Amir <Saadamir070@gmail.com>
2024-12-06 23:09:38 +0500

    image added on product detail page

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,4 +1,4 @@
-import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading,Image, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useProduct from "../hooks/useProduct";
 import ProductAttributes from "../components/ProductAttributes";
@@ -11,11 +11,22 @@ const ProductDetailPage = () => {
   if (error || !product) throw error;
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
       <GridItem>
         <Heading marginBottom={4}>{product.title}</Heading>
         <text>{product.description}</text>
         <ProductAttributes product={product} />
+      </GridItem>
+      <GridItem display="flex" justifyContent="center" alignItems="center">
+      <Image
+        src={product.images[0]}
+        alt={product.title}
+        borderRadius="md"
+        objectFit="contain"
+        maxWidth="100%"
+        height="400px"
+      />
+
       </GridItem>
 
     </SimpleGrid>
